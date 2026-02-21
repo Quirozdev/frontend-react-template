e81e5f780e493300c3a71ea583e2e2e746a105f9
Quirozdev <luisdevv232@gmail.com>
2026-02-20 21:05:40 -0700

    add framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import ThemeSwitcher from "@/features/theme/ui/ThemeSwitcher/ThemeSwitcher";
 import { Suspense } from "react";
-import { useTranslation } from "react-i18next";
+import { motion } from "motion/react";
 
 function App() {
-  const { t } = useTranslation(["common"]);
-
   return (
     <Suspense>
-      <div className="bg-background">
-        <p>{t("hello")}</p>
+      <div className="bg-background min-h-screen">
         <ThemeSwitcher />
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.95 }}
+          className="border p-1 ml-2 bg-amber-50"
+        >
+          animated
+        </motion.button>
       </div>
     </Suspense>
   );
