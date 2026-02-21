b582c19136256efedc7307994dc1c83ce3f76579
Quirozdev <luisdevv232@gmail.com>
2026-02-20 21:15:59 -0700

    add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/index.css";
-
 import "./features/translations/i18n";
-import App from "@/App";
+import { RouterProvider } from "react-router";
+import { router } from "@/app/router";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <RouterProvider router={router} />,
 );
